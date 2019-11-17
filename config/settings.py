0fafaf9da2ab9d8ca21ff46ee4b86b2e7e383462
dhdiagram@gmail.com <dhdiagram@gmail.com>
2019-11-17 10:02:16 +0900

    [update]Code Upate - Airline Ticketing System

--- config/settings.py
+++ config/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
-import DATABASES as DATABASES
-import dj_database_url
+#import DATABASES as DATABASES
+#import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
