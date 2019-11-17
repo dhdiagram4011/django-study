9ec9b53f948c1b873408da6262752d552afdc3de
dhdiagram@gmail.com <dhdiagram@gmail.com>
2019-11-17 10:00:16 +0900

    [update]Code Upate - Airline Ticketing System

--- config/urls.py
+++ config/urls.py
@@ -27,3 +27,20 @@ urlpatterns = [
     path('agreement/', include('register.urls')),
     path('ticket/', include('Ticketing.urls')),
 ]
+
+##Sampels
+#urlpatterns  = [
+#    path('',views.join, name='join'),
+#   path('',views.join_result, name='join_result'),
+#    path('',views.translate, name='translate'),
+#    path('',views.translate_result, name='translate_result'),
+#    path('',views.selecttest, name='selecttest'),
+#    path('',views.selecttest_result, name='selecttest_result'),
+#    path('',views.main, name='main'),
+#    path('',views.radiotest, name='radiotest'),
+#    path('',views.login, name='login'),
+#    path('',views.login_success, name='login_success'),
+#    path('',views.logout, name='logout'),
+#    path('',views.serverlist, name='serverlist'),
+#]
+
