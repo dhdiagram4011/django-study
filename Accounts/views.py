58510c68ed72b878f33a0f6c43b255a24046448d
dhdiagram@gmail.com <dhdiagram@gmail.com>
2019-11-20 11:22:30 +0900

    [update]Code Upate - Airline Ticketing System

--- Accounts/views.py
+++ Accounts/views.py
@@ -6,24 +6,43 @@ from django.views.generic.edit import CreateView,UpdateView,DeleteView
 from django.views.generic.detail import DetailView
 from django.http import HttpResponse
 
-
+##name = models.CharField(max_length=200)
+##birth = models.DateField('생년월일')
+##email = models.EmailField(max_length=200)
+##main_address = models.CharField(max_length=200)
+##sub_address = models.CharField(max_length=200)
+##cellphone = models.CharField(max_length=200)
+##company_name = models.CharField(max_length=200)
+##company_depart = models.CharField(max_length=200)
+##company_spot = models.CharField(max_length=200)
+##created_date = models.DateTimeField(default=timezone.now)
+##published_date = models.DateTimeField(default=timezone.now, blank=True, null=True)
 
 def RegisterStart(request):
-    if request.method == 'POST':
+    if request.method == 'GET':
+        form = RegisterForm(request.GET)
+        return render(request, 'Accounts/register.html', {'form':form})
+    elif request.method == 'POST':
         form = RegisterForm(request.POST)
-        return render(request, 'Accounts/register_success.html', {'object':object})
-    else:
-        form = RegisterForm()
-    return render(request, 'Accounts/register.html', {'form':form})
+        posts = form.save()
+        name = request.POST.get('name',None)
+        birth = request.POST.get('birth',None)
+        email = request.POST.get('email',None)
+        main_address = request.POST.get('main_address',None)
+        sub_address = request.POST.get('sub_address',None)
+        cellphone = request.POST.get('cellphone',None)
+        company_name = request.POST.get('company_name',None)
+        company_depart = request.POST.get('company_depart',None)
+        company_spot = request.POST.get('company_spot',None)
+        create_date = request.POST.get('created_date',None)
+        published_date = request.POST.get('published_date',None)
+    return redirect('RegisterSuccess')
 
 
-def AirlineLogin(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        return render(request, 'Accounts/register.html', {'object':object})
-    else:
-        form = LoginForm()
-    return render(request, 'Accounts/login.html', {'form':form})
+def RegisterSuccess(request):
+    register_lists = Register.objects.filter(published_date__lte = timezone.now()).order_by('-published_date')[:1]
+    return render(request, 'Accounts/register_success.html', {'register_lists':register_lists})
+
 
 
 #def RegisterSuccess(ListView):
@@ -40,6 +59,10 @@ def AirlineLogin(request):
 #    return render(request, 'Accounts/register.html', {'form':form})
 
 
-def RegisterSuccess(request):
-    register_lists = Register.objects.filter(published_date__lte = timezone.now()).order_by('-published_date')[:1]
-    return render(request, 'Accounts/register_success.html', {'register_lists':register_lists})
+def AirlineLogin(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        return render(request, 'Accounts/register.html', {'object':object})
+    else:
+        form = LoginForm()
+    return render(request, 'Accounts/login.html', {'form':form})
--- ftp_upload.sh
+++ ftp_upload.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rsync -avrz /Users/dohyoungkim/dango-study/* root@54.180.119.217:/var/www/django
+rsync -avrz /Users/dohyoungkim/django-study/* root@54.180.119.217:/var/www/django
 
