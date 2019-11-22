0f9114d6e19224d3ea7997449cadb58f6a906a7d
dhdiagram@gmail.com <dhdiagram@gmail.com>
2019-11-22 13:03:12 +0900

    [update]Code Upate - Airline Ticketing System

--- rev_post/views.py
+++ rev_post/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-
 from .models import *
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password, check_password
@@ -7,6 +6,7 @@ from django.contrib.auth.hashers import make_password, check_password
 import requests
 from .forms import *
 from django.http import HttpResponse
+from django.shortcuts import redirect
 
 ### input_page
 
@@ -27,11 +27,11 @@ def rev_post_list(request):
         email = request.POST.get('email',None)
         created_date = request.POST.get('created_date',None)
         published_date = request.POST.get('published_date',None)
-    return render('rev_post_result')
+    return redirect('rev_post_result')
 
 
 def rev_post_result(request):
-    rev_post_lists = rev_post.objects.filter(publisehd_date__lte=timezone.now()).order_by('-published_date')[:1]
+    rev_post_lists = rev_post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')[:1]
     return render(request, 'rev_post/main_result.html', {'rev_post_lists':rev_post_lists})
 
 
