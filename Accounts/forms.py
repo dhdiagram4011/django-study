4fe98537df4dde81e36dcc4de4f4c239b34e6673
golbkim <dhdiagram@gmail.com>
2019-11-24 22:47:22 +0900

    [update]Code Upate - Airline Ticketing System

--- Accounts/forms.py
+++ Accounts/forms.py
@@ -3,12 +3,27 @@ from django import forms
 from .models import *
 
 class RegisterForm(forms.ModelForm):
+    password = forms.CharField(label='passowrd', widget=forms.PasswordInput)
+    repeat_password = forms.CharField(label='repeat_passowrd', widget=forms.PasswordInput)
+    name = forms.CharField(label='name')
+    birth = forms.DateField(label='birth')
+    email = forms.EmailField(label='email')
+    main_address = forms.CharField(label='main_address')
+    sub_address = forms.CharField(label='sub_address')
+    cellphone = forms.CharField(label='cellular_phone')
+    company_name = forms.CharField(label='company_name')
+    company_depart = forms.CharField(label='company_depart')
+    company_spot = forms.CharField(label='company_spot')
+    created_date = forms.DateTimeField(label='created_date')
+    published_date = forms.DateTimeField(label='published_date')
+
     def clean_repeat_password(self):
         cd = self.cleaned_data
         
         if cd['password'] != cd['repeat_password']:
             raise forms.ValidationError('passowrd not matched!')
         return cd['repeat_password']
+
 
     class Meta:
         model = Register
