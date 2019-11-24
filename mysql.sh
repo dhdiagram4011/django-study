34702a77d5a2f0a4643c478309d8acfee7e699af
golbkim <dhdiagram@gmail.com>
2019-11-24 21:45:31 +0900

    [update]Code Upate - Airline Ticketing System

--- mysql.sh
+++ mysql.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-###mysql --host=35.239.125.166 --user=root --password
-mysql --host=ats.c0hryaktyqa9.ap-northeast-2.rds.amazonaws.com --user=dhdiagram --password
+/usr/local/bin/mysql --host=ats.c0hryaktyqa9.ap-northeast-2.rds.amazonaws.com --user=dhdiagram --password
