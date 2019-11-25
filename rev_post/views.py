08fedbae147d30a85fd021be86104a852b7bcb7d
dhdiagram@gmail.com <dhdiagram@gmail.com>
2019-11-25 10:14:09 +0900

    [update]Code Upate - Airline Ticketing System

--- rev_post/views.py
+++ rev_post/views.py
@@ -25,11 +25,11 @@ def rev_post_list(request):
         return render(request,'rev_post/main.html', {'form':form})
     elif request.method == 'POST':
         form = rev_post_list_Form(request.POST)
-        posts = form.save()
         id = request.POST.get('id',None)
         email = request.POST.get('email',None)
         created_date = request.POST.get('created_date',None)
         published_date = request.POST.get('published_date',None)
+        posts = form.save()
     return redirect ('rev_post_result')
 
 
