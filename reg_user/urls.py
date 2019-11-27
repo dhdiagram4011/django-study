c2672a0e3431a6c4171fd81859795bc6d417ff5f
dhdiagram@gmail.com <dhdiagram@gmail.com>
2019-11-27 09:18:49 +0900

    [update]Code Upate - Airline Ticketing System

--- reg_user/urls.py
+++ reg_user/urls.py
@@ -12,7 +12,7 @@ reg_userdetail = reg_userViewSet.as_view(
 urlpatterns = [
     path('main/', reg_user_list, name="reg_user_list"),
     path('result/', reg_user_result, name="reg_user_result"),
-    path('auth/register', RegistraionAPI.as_view()),
-    path('auth/login', LoginAPI.as_view()),
-    path('auth/users', UserAPI.as_view()),
+    path('auth/register/', RegistraionAPI.as_view()),
+    path('auth/login/', LoginAPI.as_view()),
+    path('auth/user/', UserAPI.as_view()),
 ]
