bf4116523b722b28652a21c30bc86902bf25bdc0
dhdiagram@gmail.com <dhdiagram@gmail.com>
2019-12-21 13:43:11 +0900

    admin page insert model

--- airline-reservation-api/AirlineReservationApi/admin.py
+++ airline-reservation-api/AirlineReservationApi/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+
+from .models import *
+
+
+class RegisterOption(admin.ModelAdmin):
+    list_display = ['register_created','name','password','main_address','sub_address','company','depart','position']
+
+admin.site.register(Register, RegisterOption)
+
+
+
+class AirscheduleOption(admin.ModelAdmin):
+    list_display = ['schedule_created','arrival_time','departure_time','navigation_section','operating_time','airplane_type']
+
+admin.site.register(Airschedule,AirscheduleOption)
+
+
+
+
