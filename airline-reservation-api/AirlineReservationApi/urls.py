a350992f3b2c995721dca738e0508975204ed4b1
golbkim <dhdiagram@gmail.com>
2019-12-25 11:49:01 +0900

    code sync

--- airline-reservation-api/AirlineReservationApi/urls.py
+++ airline-reservation-api/AirlineReservationApi/urls.py
@@ -15,7 +15,6 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('user/', register_list, name='register_list'),
     path('user_list/<int:pk>', register_detail, name='register_detail'),
-
     path('schedule/', schedule_list, name='schedule_list'),
     path('schedule_list/<int:pk>', schedule_detail, name='schedule_detail'),
 ]
