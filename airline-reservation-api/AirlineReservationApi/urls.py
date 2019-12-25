b0e2f63acff9847f33b9f173f37b0846c9db5c34
golbkim <dhdiagram@gmail.com>
2019-12-25 11:42:12 +0900

    settings.py modify - installed apps list delete

--- airline-reservation-api/AirlineReservationApi/urls.py
+++ airline-reservation-api/AirlineReservationApi/urls.py
@@ -2,6 +2,7 @@ from django.urls import include, path
 from rest_framework import routers
 from .views import *
 from django.contrib import admin
+from django.urls import reverse
 
 admin.site.site_header = 'Reservation Ticketing System API System'
 
@@ -11,13 +12,12 @@ router.register(r'groups', GroupViewSet)
 
 urlpatterns = [
     path('webmaster/', admin.site.urls),
-    #path('',include(router.urls)),
     path('', index, name=index),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('user/', register_list, name='register_list'),
     path('user_list/<int:pk>', register_detail, name='register_detail'),
-
     path('schedule/', schedule_list, name='schedule_list'),
     path('schedule_list/<int:pk>', schedule_detail, name='schedule_detail'),
+    #path('',include(router.urls)),
 ]
 
--- airline-reservation-api/AirlineReservationApi/views.py
+++ airline-reservation-api/AirlineReservationApi/views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import User,Group
 from rest_framework import viewsets
 from .serializers import UserSerializer, GroupSerializer
-
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.renderers import JSONRenderer
