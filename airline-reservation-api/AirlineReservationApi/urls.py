6780da7c14f4cf8b81450ada9f763267b2f56e47
golbkim <dhdiagram@gmail.com>
2019-12-25 11:16:45 +0900

    readme file add

--- airline-reservation-api/AirlineReservationApi/urls.py
+++ airline-reservation-api/AirlineReservationApi/urls.py
@@ -10,7 +10,7 @@ router.register(r'users', UserViewSet)
 router.register(r'groups', GroupViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('webmaster/', admin.site.urls),
     path('',include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('user/', register_list, name='register_list'),
